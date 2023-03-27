bfb61bc3d6036caed910057dd31f1ca1c2e1ea86
huy9633 <92261433+huy9633@users.noreply.github.com>
2023-03-27 12:46:24 +0700

    Add files via upload

---
+++ ProjectCTDL1/Display.h
@@ -0,0 +1,45 @@
+#ifndef __DISPLAY__
+#define __DISPLAY__
+#include"GoodManagement.h";
+#include"goods.h";
+
+using namespace std;
+	
+void HT(vector<goods> hh) {
+	int n = hh.size() + 3;
+	if (hh[0].seri == "") {
+		hh.clear();
+	}
+	int num = hh.size();
+	if (hh[num-1].seri == "") {
+		num--;
+	}	
+	if (hh.empty()) {
+		vietchuoi(50, 1, "[Hien Tai Chua Co Hang Hoa]", 12);
+		Sleep(1500);
+		return;
+	}
+	vietchuoi(50, 1, "Nhan [ESC] de tro ve ", 12); 
+	bangsanpham(0, 2, n);
+	for (int i = 0; i < num; i++) {
+		int y = 5 + i;
+		gotoxy(2, y); cout << i + 1;
+		gotoxy(5, y); cout << hh[i].seri;
+		gotoxy(16, y); cout << hh[i].name;
+		gotoxy(44, y); cout << hh[i].place;
+		gotoxy(58, y); cout << hh[i].color;
+		gotoxy(73, y); cout << hh[i].price;
+		gotoxy(87, y); cout << hh[i].importDate[0] << "/" << hh[i].importDate[1] << "/" << hh[i].importDate[2];
+		gotoxy(106, y); cout << hh[i].amount;
+	}
+	int h = keyboard();
+	if (h == 4) {
+		system("cls");
+	}
+	else {
+		HT(hh);
+	}
+}
+
+#endif
+
